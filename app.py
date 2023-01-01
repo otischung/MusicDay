07ae1dda66adedff6b21d1540f9f7e4cea9899a5
kingslayer929 <percylin0929@gmail.com>
2023-01-01 21:58:45 +0800

    five standard analiize finish

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 from flask_cors import CORS
+import os
+import analize
+
 
 app = Flask(__name__)
 CORS(app)
@@ -13,13 +16,23 @@ def home():
 def upload_page():
     return render_template("upload.html")
 
-@app.route("/api/upload", methods=['POST'])
+@app.route("/result", methods=['POST'])
 def upload():
     if request.method != 'POST':
         redirect('/upload')
+    
+    
+    file = request.files['upload_file']
+    filepath = os.path.join(".", "tmp", file.filename)
+    file.save(filepath)
+    
     # 呼叫音高分析函式
+    result = analize.five(filepath)
+    print(result)
     
     # 呼叫比對函式
+    
+    os.unlink(filepath)
     
     return render_template("result.html")
 
