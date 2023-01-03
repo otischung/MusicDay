40ab4b4f25033f7b59848584f5157ea62c651f66
kingslayer929 <percylin0929@gmail.com>
2023-01-04 02:47:54 +0800

    merge recommendation to backend

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, render_template, redirect, url_for, session
 from flask_cors import CORS
 import os
 import analize
-
+import recommendation_system
+import librosa
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -27,12 +29,21 @@ def upload():
     file.save(filepath)
     
     # 呼叫音高分析函式
-    result = analize.five(filepath)
-    print(result)
-    
-    # 呼叫比對函式
+    user_pitch = analize.five(filepath)
+    # print(user_pitch)
     
     os.unlink(filepath)
+    
+    # 呼叫比對函式
+    recommend = recommendation_system.recommendation(user_pitch)
+    # print(recommend)
+    
+    result = []
+    result.append(dict([('user_pitch', [librosa.midi_to_note(user_pitch[x]) for x in range(0, 5)])]))
+    result.append(dict([('recommend', recommend)]))
+    
+    result = json.dumps(result)
+    # print(result)
     
     return render_template("result.html")
 
