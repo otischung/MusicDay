60a47793e91ff1187a804c3b8b79044a254a3e62
Lyon Lu <lyonlu13@gmail.com>
2023-01-05 03:11:20 +0800

    final system

--- app.py
+++ app.py
@@ -1,53 +1,81 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, send_from_directory, redirect, send_file, session
 from flask_cors import CORS
 import os
-import analize
 import recommendation_system
+import reference_pitch
 import librosa
 import json
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="./dist/static")
 CORS(app)
 
 
+def root_dir():  # pragma: no cover
+    return os.path.abspath(os.path.dirname(__file__))
+
+
 @app.route("/")
+@app.route("/about")
+@app.route("/analysis")
+@app.route("/analysis/mode")
+@app.route("/analysis/upload")
+@app.route("/analysis/result")
 def home():
-    return render_template("home.html")
-    
-@app.route("/upload")
-def upload_page():
-    return render_template("upload.html")
+    print(os.path.join(root_dir(), "index.html"))
+    return send_file(os.path.join(root_dir(), "./dist/index.html"))
+
+
+
+@app.route("/static/<path:path>")
+def static_folder(path):
+    print(path)
+    return send_from_directory("./dist/static/", path)
+
+@app.route("/assets/<path:path>")
+def assets_folder(path):
+    return send_from_directory("./dist/assets/", path)
+
+@app.route('/Cover/<path:path>')
+def get_resource(path):  # pragma: no cover
+    complete_path = os.path.join(root_dir()+"/cover/", path)
+    return send_file(complete_path, mimetype='image/png')
+
 
 @app.route("/api/result", methods=['POST'])
 def upload():
     if request.method != 'POST':
         redirect('/upload')
-    
-    
+
     file = request.files['upload_file']
     filepath = os.path.join(".", "tmp", file.filename)
     file.save(filepath)
-    
+
     # 呼叫音高分析函式
-    user_pitch = analize.five(filepath)
+    user_pitch = reference_pitch.ref_pitch(filepath, fmin="C2", fmax="C6")
     # print(user_pitch)
-    
+
     os.unlink(filepath)
-    
+
     # 呼叫比對函式
     recommend = recommendation_system.recommendation(user_pitch)
+    recommend_a8 = recommendation_system.recommendation(
+        [user_pitch[x]+12 for x in range(0, 5)])
+    recommend_m8 = recommendation_system.recommendation(
+        [user_pitch[x]-12 for x in range(0, 5)])
     # print(recommend)
-    
-    result = []
-    result.append(dict([('user_pitch', [librosa.midi_to_note(user_pitch[x]) for x in range(0, 5)])]))
-    result.append(dict([('recommend', recommend)]))
-    
+
+    result = dict()
+    result['user_pitch_visualize'] = [
+        librosa.midi_to_note(user_pitch[x]) for x in range(0, 5)]
+    result['user_pitch'] = user_pitch
+    result['recommends'] = []
+    result['recommends'].append(recommend_m8)
+    result['recommends'].append(recommend)
+    result['recommends'].append(recommend_a8)
+
     result = json.dumps(result)
-    # print(result)
-    
-    return render_template("result.html")
+    return result
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
---
+++ reference_pitch.py
@@ -0,0 +1,133 @@
+"""
+Provides some librosa functions
+"""
+import sys
+
+import librosa
+import numpy as np
+
+
+def ref_pitch(src: str, fmin: int | str = "G2", fmax: int | str = "C6", noise_gate: bool = True, sr: int = 44100, frame_len: int = 8000, num_cont_frame: int = 5) -> list:
+    """
+    Get the five reference pitch of given source audio file.
+
+    You can specify the minimum and maximum frequencies by MIDI integer value
+    or note name string, e.g. "C#3", "Ab4".
+
+    Parameters
+    ----------
+    src: str
+        Input audio file path.
+
+    fmin: int|str
+        The minimum frequency. The default value is G2 (43)
+
+    fmax: int|str
+        The maximum frequency. The default value is C6 (84)
+
+    noise_gate: bool
+        The default value is True. It will detect blanks and delete the blank parts.
+
+    sr: int
+        Sample rate. The default value is 44.1kHz, which is the standard of CD quality.
+
+    frame_len: int
+        This value determines that how many frames will be used in FFT. The default value is 8000.
+        This value also determines the segment length. Take our default values for example,
+        the segment length is 8000 / 44100 * 1000 = 181.4059 ms.
+
+    num_cont_frame: int
+        This value sets the numbers of continuous frame that are all same pitch.
+        The default value is 5 frames.
+        For example, [69, 69, 69, 69, 69, 70, ...], then 69 (A4) will be accepted.
+        On the contrary, [69, 70, 71, 72, 73, ...], then all pitches will be rejected.
+
+    Returns
+    -------
+    out: list
+        The list containing five reference pitch. The order is [low, avg. low, avg., avg high, high].
+
+    Raises
+    ------
+    FileNotFoundError
+        If the given source audio file doesn't exist.
+
+    ParameterError
+        If the input frequencies are not in valid note format.
+    """
+    y0, sr = librosa.load(src, sr=sr)  # y0 is the time domain of the music read in RAM; sr is the sample rate.
+    # ------ Perform Noise Gate -----
+    if noise_gate:
+        n_fft = frame_len
+        S = librosa.stft(y0, n_fft=n_fft, hop_length=n_fft // 2)
+        D = librosa.amplitude_to_db(np.abs(S), ref=np.max)
+        np.max(abs(D))
+
+        nonMuteSections = librosa.effects.split(y0, top_db=20)
+        # y will be the voice without blank.
+        y = []
+
+        for sliced in nonMuteSections:
+            y.extend(y0[sliced[0]:sliced[1]])
+    else:
+        y = y0
+    # -------------------------------
+
+    # Transfer to numpy array.
+    y = np.array(y)
+
+    if type(fmin) == str:
+        fmin = librosa.note_to_midi(fmin)
+    if type(fmax) == str:
+        fmax = librosa.note_to_midi(fmax)
+    # This will take few times.
+    # f0 is the detected fundamental frequencies based on time sequence.
+    f0, voiced_flag, voiced_probabilities = librosa.pyin(
+        y, sr=sr, frame_length=frame_len,
+        fmin=librosa.midi_to_hz(fmin), fmax=librosa.midi_to_hz(fmax))
+
+    appear = []
+    midi_note = np.around(librosa.hz_to_midi(f0))
+    i = len(midi_note) - 1
+
+    histogram = [0] * 128
+    while i >= 0:
+        if midi_note[i] != 'nan':
+            is_a_note = True
+            for t in range(num_cont_frame):
+                if midi_note[i - t - 1] != midi_note[i]:
+                    is_a_note = False
+                    break
+            if is_a_note:
+                appear.append(midi_note[i])
+                histogram[int(midi_note[i])] += 1
+                i -= num_cont_frame
+            else:
+                i -= 1
+
+    if len(appear) == 0:
+        print(f"Error, the music {src} has zero note.", file=sys.stderr)
+        return [0, 0, 0, 0, 0]
+
+    # This will take few times.
+    # appear.sort()
+
+    # Unstable counting sort
+    appear_sort = []
+    for i in range(128):
+        for j in range(histogram[i]):
+            appear_sort.append(i)
+
+    # 5 index
+    # print("average", librosa.midi_to_note(appear[(int)(len(appear) / 2)]))
+    # print("average high", librosa.midi_to_note(appear[(int)(3 * len(appear) / 4)]))
+    # print("average low", librosa.midi_to_note(appear[(int)(len(appear) / 4)]))
+    # print("low", librosa.midi_to_note(appear[(int)(len(appear) / 100)]))
+    # print("high", librosa.midi_to_note(appear[(int)(98 * len(appear) / 100)]))
+    low = int(appear_sort[int(len(appear_sort) / 100)])
+    avg_low = int(appear_sort[int(len(appear_sort) / 4)])
+    avg = int(appear_sort[int(len(appear_sort) / 2)])
+    avg_high = int(appear_sort[int(3 * len(appear_sort) / 4)])
+    high = int(appear_sort[int(98 * len(appear_sort) / 100)])
+    # high = int(appear[int(len(appear) - 2)])
+    return [low, avg_low, avg, avg_high, high]
